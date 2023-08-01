2323ddfa10743fce61a66de069bc9ab94184cdb0
seredganyha <karix05@yandex.ru>
2023-08-01 23:39:30 +0300

    init npm project and add ts, webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: ['ts-loader'],
+            },
+        ],
+    },
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+        static: {
+            directory: path.join(__dirname, "./")
+        },
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Spiral text',
+            template: path.resolve(__dirname, './src/index.html'), 
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    }
+}
